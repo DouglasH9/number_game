c14c6175c0f46c2f8274a050ab3f1f8341e0fea9
Doug Herman <douglash9882@gmail.com>
2021-09-06 22:30:39 -0500

    messed with css

--- server.py
+++ server.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, session, redirect, request
 app = Flask(__name__)
+import random
 app.secret_key = 'omar comin!'
 
 @app.route('/')
 def render_page():
+    if 'number' not in session:
+        session['number'] = random.randint(1,100)
     return render_template('index.html')
 
-@app.route('/render', methods=['POST'])
+@app.route('/guess', methods=['POST'])
 def show_result():
-    session['number']=request.form['number']
-    
+    session['guess']= int(request.form['guess'])
+    return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session.clear()
     return redirect('/')
 
 if __name__=='__main__':
